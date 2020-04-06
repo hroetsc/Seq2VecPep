f5f71094b948b74293d0ea89d2915250cb594295
Hanna P. Roetschke <hanna.roetschke@web.de>
2020-04-06 08:49:34 +0200

    Add files via upload

--- Seq2Vec/src/sequence_repres.R
+++ Seq2Vec/src/sequence_repres.R
@@ -51,6 +51,12 @@ colnames(indices) = c("subword", "word_ID")
 indices$subword = toupper(indices$subword)
 
 ### MAIN PART ###
+if (ncol(indices) == 3){
+  indices$V1 = NULL
+}
+colnames(indices) = c("subword", "word_ID")
+indices$subword = toupper(indices$subword)
+
 # merge indices and weights
 colnames(weight_matrix)[1] = "word_ID"
 weight_matrix$word_ID = weight_matrix$word_ID + 1
@@ -63,13 +69,6 @@ weights = unique(weights)
 sequences.master = left_join(sequences, words)
 sequences.master = na.omit(sequences.master)
 
-# add embeddings to master table and calculate sequence representation
-# find tokens for every unique sequence (Accession) and sum up all dimensions of the tokens
-# to get the respective embedding dimension
-# thereby, normalize token embeddings by TF-IDF score
-
-print("CALCULATE NUMERIC REPRESENTATION OF EVERY SEQUENCE")
-
 # define function (that allows parallel searching) for tokens in weight matrix
 find_tokens = function(token = ""){
   if (token %in% weights$subword) {
@@ -80,21 +79,9 @@ find_tokens = function(token = ""){
   }
 }
 
-# define function that finds TF-IDF score for token in correct sequence
-find_TF.IDF = function(sequence = "", tokens = "") {
-  scores = rep(NA, length(tokens))
-  tmp = TF_IDF[which(TF_IDF$Accession == sequence), ]
-
-  for (t in 1:length(tokens)) {
-    if (tokens[t] %in% tmp$token) {
-      scores[t] = tmp[which(tmp$token == tokens[t]), "tf_idf"]
-    }
-  }
-  return(scores)
-}
-
+# create matrix that will contain average of token embeddings
 sequence.repres = as.data.frame(matrix(ncol = ncol(sequences.master)+ncol(weights)-2,
-                                      nrow = nrow(sequences.master))) # contains vector representation for every sequence
+                                       nrow = nrow(sequences.master)))
 colnames(sequence.repres) = c(colnames(sequences.master), seq(1,ncol(weights)-2,1))
 dim_range = c(ncol(sequences.master)+1, ncol(sequence.repres))
 sequence.repres[, c(1:(dim_range[1]-1))] = sequences.master
@@ -103,26 +90,17 @@ sequence.repres[, c(1:(dim_range[1]-1))] = sequences.master
 progressBar = txtProgressBar(min = 0, max = nrow(sequences.master), style = 3)
 for (i in 1:nrow(sequences.master)) {
   setTxtProgressBar(progressBar, i)
-
+  
   # build temporary table that contains all tokens and weights for the current sequences
   current_tokens = t(str_split(sequences.master$tokens[i], pattern = " ", simplify = T))
   tmp = as.data.frame(matrix(ncol = ncol(weights)-2, nrow = nrow(current_tokens)))
   tmp[, "token"] = current_tokens
-
+  
   # find embeddings for every token in tmp
   for (r in 1:nrow(tmp)) {
     tmp[r,c(1:(ncol(weights)-2))] = find_tokens(paste(tmp[r, ncol(tmp)]))
   }
-
-  # extract TF-IDF scores for all tokens
-  tmp[, "TF_IDF"] = find_TF.IDF(sequence = sequences.master$Accession[i],
-                                tokens = tmp$token)
-
-  # multiply token embeddings by their TF-IDF scores
-  tmp[, c(1:(ncol(tmp)-2))] = tmp[, c(1:(ncol(tmp)-2))] * tmp$TF_IDF
-  tmp$TF_IDF = NULL
-  tmp$token = NULL
-
+  
   # only proceed if embeddings for every token are found, otherwise discard sequence
   if (!any(is.na(tmp))) {
     # calculate mean of every token dimension to get sequence dimension
@@ -130,13 +108,12 @@ for (i in 1:nrow(sequences.master)) {
       tmp[,c] = as.numeric(as.character(tmp[,c]))
     }
     sequence.repres[i, c(dim_range[1]:dim_range[2])] = colSums(tmp[,c(1:(ncol(weights)-2))]) / nrow(tmp)
-
+    
   } else {
     sequence.repres[i, c(dim_range[1]:dim_range[2])] = rep(NA, length(c(dim_range[1]:dim_range[2])))
   }
 }
 
-print("FORMAT OUTPUT")
 sequence.repres = na.omit(sequence.repres)
 sequence.repres = unique(sequence.repres)
 
