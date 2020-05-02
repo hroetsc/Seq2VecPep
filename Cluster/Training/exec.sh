d81e9b643ca2c09afffd6eeee2c2530232a7536d
Hanna P. Roetschke <hanna.roetschke@web.de>
2020-05-02 08:34:49 +0200

    Add files via upload

--- Cluster/Training/exec.sh
+++ Cluster/Training/exec.sh
@@ -2,12 +2,16 @@
 
 module purge
 
-module load cuda10.0/toolkit/10.0.130
+
 module load conda/4.3.30
 
 source activate seq2vec
 
+module load cuda10.0/toolkit/10.0.130
+module load cudnn/10.0v7.6.3
+
+
 snakemake --unlock
-snakemake --use-conda -j 200 --cluster "sbatch -A all -o hp_training-%J.out -p gpu -C scratch2 -N 10 -n 200 -G 30 -t 2-00:00:00 --mail-type=END --mail-user=hanna.roetschke@mpibpc.mpg.de"
+snakemake --use-conda -j 300 --cluster "sbatch -A all -o hp_training-%J.out -p gpu -C scratch2 -N 15 -n 300 --tasks-per-node 20 -G 30 -t 2-00:00:00 --mail-type=END --mail-user=hanna.roetschke@mpibpc.mpg.de"
 
 source deactivate
